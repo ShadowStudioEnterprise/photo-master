72d4d2c309c0c3023e90be28b9c6fd0bc1b17290
ShadowStudioEnterprise <shadowstudioenterprise@gmail.com>
2021-10-20 09:03:22 +0200

    V3

--- utils/utils.php
+++ utils/utils.php
@@ -1,10 +1,17 @@
-<?php 
-    function esOpcionMenuActiva(string $option){
-
+<?php
+function esOpcionMenuActiva(string $option): bool{
+    if (strpos($_SERVER["REQUEST_URI"], "/". $option) === 0 ){
+        return true;
+    }elseif ("/" === $_SERVER["REQUEST_URI"] && ("index" == $option)){
+        return true;
+    }else   
+        return false;
+}
+function  existeOpcionMenuActivaEnArray(array $options): bool{
+    foreach ($options as $option){
+        if (esOpcionMenuActiva($option)) {
+            return true;
+        }
     }
-
-    function  existeOpcionMenuActivaEnArray(array $options){
-
-    }
-
-?>
+    return false;
+}
